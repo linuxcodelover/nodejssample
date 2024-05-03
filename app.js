e75d61c77fb0ffba493680d17e0a4194c103a828
linuxcodelover <nikulthakor6458@gmail.com>
2024-05-03 14:26:45 +0530

    initial

---
+++ app.js
@@ -0,0 +1,8 @@
+const express= require('express');
+const dotenv = require('dotenv');
+exports.app= express();
+
+
+dotenv.config({path:"./data/config.env"})
+
+ 
---
+++ server.js
@@ -0,0 +1,15 @@
+const {app}=require("./app")
+
+app.get("/", (req,res)=>{
+    res.send("<h1>Workin Fine</h1>")
+})
+app.get("/user", (req,res)=>{
+    res.status.json({
+        users:[],
+        success:false
+    })
+})
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
